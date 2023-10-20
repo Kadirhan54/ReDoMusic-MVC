40b89ca71ab2be470c9ebc017ff1ef51af4276fe
Kadirhan54 <kadirhansaglam5454@gmail.com>
2023-10-21 00:40:07 +0300

    Add project files.

---
+++ Core/ReDoMusic.Domain/Entities/Instrument.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReDoMusic.Domain.Common;
+using ReDoMusic.Domain.Enums;
+
+namespace ReDoMusic.Domain.Entities
+{
+    public class Instrument : EntityBase<Guid>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Brand Brand { get; set; }
+        public decimal Price { get; set; }
+        public Color Color { get; set; }
+        public string Barcode { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
---
+++ Infrastructure/ReDoMusic.Persistence/Contexts/ReDoMusicDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReDoMusic.Domain.Entities;
+
+namespace ReDoMusic.Persistence.Contexts
+{
+    public class ReDoMusicDbContext : DbContext
+    {
+        public DbSet<Instrument> Instruments { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(Configurations.GetString("ConnectionStrings:PostgreSQL"));
+        }
+    }
+}
